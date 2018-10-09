2c26292932004a331b6b24fc0bffafe86521ae68
Lomztein <lomztein2@gmail.com>
2018-10-09 13:28:49 +0200

    Commented CommandWord.java

--- src/depressionsspillet/worldofzuul/CommandWord.java
+++ src/depressionsspillet/worldofzuul/CommandWord.java
@@ -1,5 +1,8 @@
 package depressionsspillet.worldofzuul;
 
+// An enum is a funky one. This is a speciala type of class that can be used to define an amount of constants.
+// Each constant in this case, such as GO or HELP is declared using a constructor just like any other class.
+// These can then be used elsewhere as constants by referencing them using CommandWord.GO for example, and their attributes and methods can then be accessed.
 public enum CommandWord
 {
     GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?");
